6aa3844231ebea49ba9606655458c2ef1ff766bc
neilpeterson <neilpeterson@outlook.com>
2016-02-27 13:39:22 -0800

    adding POSH sample

---
+++ windows-custom-script-simple/custom-script-powershell.ps1
@@ -0,0 +1,8 @@
+﻿$cred = Get-Credential
+$script = "https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/windows-custom-script-simple/Create-File.ps1"
+$run = "Create-File.ps1"
+$name = "scripttest"
+$location = "West US"
+
+Login-AzureRmAccount -Credential $cred
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $name -VMName $name -Name $name -FileUri $script -Run $run -Location $location
