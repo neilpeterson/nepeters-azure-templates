e6581a4f720d1898a2a3b49953e64ada01aced7a
neilpeterson <neilpeterson@outlook.com>
2016-02-29 21:30:53 -0800

    updates

--- windows-custom-script-simple/support-scripts/custom-script-powershell.ps1
+++ windows-custom-script-simple/support-scripts/custom-script-powershell.ps1
@@ -1,8 +1,10 @@
 ﻿$cred = Get-Credential
 $script = "https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/windows-custom-script-simple/support-scripts/Create-File.ps1"
 $run = "Create-File.ps1"
-$name = "scripttest"
+$name = "custom-script"
+$rgroup = ""
+$vmname = ""
 $location = "West US"
 
 Login-AzureRmAccount -Credential $cred
-Set-AzureRmVMCustomScriptExtension -ResourceGroupName $name -VMName $name -Name $name -FileUri $script -Run $run -Location $location
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $grroup -VMName $vmname -Name $name -FileUri $script -Run $run -Location $location
