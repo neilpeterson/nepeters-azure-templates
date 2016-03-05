33c88389c17dd7b365e483585e0feaf12d9965c9
Neil Peterson <neilpeterson@outlook.com>
2016-03-04 21:00:22 -0800

    Update windows-swarm-setup.ps1

--- windows-container-swarm/support-scripts/windows-swarm-setup.ps1
+++ windows-container-swarm/support-scripts/windows-swarm-setup.ps1
@@ -1,9 +1,7 @@
 $swarmpath = 'c:\swarm'
 $ip = (Get-NetIPAddress | where {$_.IPAddress -Like '10.0.0.*'})[0]
-$realip = $ip.IPAddress
-$ad = $realip + ":2375"
-$netsh = "netsh interface portproxy add v4tov4 listenport=2375 listenaddress=$realip connectport=2375 connectaddress=127.0.0.1"
-$swarm = "$swarmpath\swarm.exe join --advertise=$ad consul://172.16.0.5:8500/nodes"
+$netsh = "netsh interface portproxy add v4tov4 listenport=2375 listenaddress=$($ip.IPAddress) connectport=2375 connectaddress=127.0.0.1"
+$swarm = "$swarmpath\swarm.exe join --advertise=$($ip.IPAddress):2375 consul://172.16.0.5:8500/nodes"
 
 if (!(Test-Path -Path $swarmpath)) {
     New-Item -ItemType Directory -Path $swarmpath
