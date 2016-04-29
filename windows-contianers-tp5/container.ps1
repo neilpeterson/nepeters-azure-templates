3728a79ae6b6c0a02934c0232585b555afa204ee
Neil Peterson <neilpeterson@outlook.com>
2016-04-29 13:43:48 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -4,11 +4,11 @@ if ($feature.Installed -eq $false) {
     
     Install-WindowsFeature containers 
     
-    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\container.ps1"
-    $trigger = New-ScheduledTaskTrigger -AtLogOn
-    Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest | Out-Null
+    #$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\container.ps1"
+    #$trigger = New-ScheduledTaskTrigger -AtLogOn
+    #Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest | Out-Null
     Restart-Computer -Force      
 }
 
-Unregister-ScheduledTask -TaskName contianers -Confirm:$false
+#Unregister-ScheduledTask -TaskName contianers -Confirm:$false
 New-Item -ItemType File c:\test.txt -Force
