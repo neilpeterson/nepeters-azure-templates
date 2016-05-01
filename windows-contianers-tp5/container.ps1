56988ea06dbd351f297661092b170f99e1840010
Neil Peterson <neilpeterson@outlook.com>
2016-04-30 21:31:33 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -7,7 +7,7 @@
     $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.8\Downloads\0\container.ps1"
     $trigger = New-ScheduledTaskTrigger -AtLogOn
     Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
-    Restart-Computer -Force      
+    #Restart-Computer -Force      
 #}
 
 # Unregister-ScheduledTask -TaskName contianers -Confirm:$false
