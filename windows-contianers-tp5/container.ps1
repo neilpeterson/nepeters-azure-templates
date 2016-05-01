15c889a1da82a53e1ea917f6ded952f3de7e6d73
Neil Peterson <neilpeterson@outlook.com>
2016-04-30 22:57:58 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -1,5 +1,13 @@
-Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Install-ContainerHost/Install-ContainerHost.ps1 -OutFile c:\install-containerhost.ps1
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\install-containerhost.ps1"
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -TaskName "scriptcontianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
-Restart-Computer -Force
+$feature = Get-WindowsFeature -Name containers
+
+if ($feature.Installed -eq $false) {
+    
+    Install-WindowsFeature containers 
+    
+    "Install-PackageProvider ContainerImage -Force;Install-ContainerImage -Name WindowsServerCore;Unregister-ScheduledTask -TaskName scriptcontianers -Confirm:$false" > c:\images.ps1
+    
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\images.ps1"
+    $trigger = New-ScheduledTaskTrigger -AtLogOn
+    Register-ScheduledTask -TaskName "scriptcontianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
+    Restart-Computer -Force      
+}
