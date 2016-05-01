334427c7c31b6f75661b3458b3cc51af99218c8e
Neil Peterson <neilpeterson@outlook.com>
2016-05-01 10:54:34 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -1,13 +1,29 @@
+param (
+[string]$adminUser
+)
+
+
 $feature = Get-WindowsFeature -Name containers
 
 if ($feature.Installed -eq $false) {
     
-    Install-WindowsFeature containers 
-    
-    "Install-PackageProvider ContainerImage -Force;Install-ContainerImage -Name WindowsServerCore;Unregister-ScheduledTask -TaskName scriptcontianers -Confirm:$false" > c:\images.ps1
+    Install-WindowsFeature containers    
+
+    Install-Script > c:\images.ps1
     
     $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\images.ps1"
     $trigger = New-ScheduledTaskTrigger -AtLogOn
-    Register-ScheduledTask -TaskName "scriptcontianers" -Action $action -Trigger $trigger -RunLevel Highest -User "neillocal" | Out-Null
+    Register-ScheduledTask -TaskName "scriptcontianers" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null
     Restart-Computer -Force      
+}
+
+function install-script {
+"# TP5 Contianer Installation`r`n
+    # Install Windows Server Core Image`r`n
+    Install-PackageProvider ContainerImage -Force`r
+    Install-ContainerImage -Name WindowsServerCore`r`n    
+    # Install Docker daemon and client`r`n
+    Invoke-WebRequest https://aka.ms/tp5/Update-Container-Host -OutFile update-containerhost.ps1`r
+    .\update-containerhost.ps1`r`n
+    Unregister-ScheduledTask -TaskName scriptcontianers -Confirm:$false"
 }
