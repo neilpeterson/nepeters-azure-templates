ccc4b4eaca6bf0d288ee7566c0a49486d8362be9
Neil Peterson <neilpeterson@outlook.com>
2016-04-30 22:16:25 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -1,27 +1,5 @@
-$feature = Get-WindowsFeature -Name containers
-
-if ($feature.Installed -eq $false) {
-    
-    Install-WindowsFeature containers 
-    
-    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.8\Downloads\0\container.ps1"
-    $trigger = New-ScheduledTaskTrigger -AtLogOn
-    Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
-    Restart-Computer -Force      
-}
-
-
-
-if (!(Get-ContainerImage -Name windowsservercore)) {
-
-    Install-PackageProvider ContainerImage -Force
-    Install-ContainerImage -Name WindowsServerCore
-}
-
-
-Invoke-WebRequest https://aka.ms/tp5/Update-Container-Host -OutFile update-containerhost.ps1
-.\update-containerhost.ps1
-sleep 300
-New-Item c:\test.txt
-
-Unregister-ScheduledTask -TaskName contianers -Confirm:$false
+Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Install-ContainerHost/Install-ContainerHost.ps1 -OutFile c:\install-containerhost.ps1
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\install-containerhost.ps1"
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
+Restart-Computer -Force
