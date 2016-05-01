a8bbc45709bf81a4c941b49e11b6e8826df834db
Neil Peterson <neilpeterson@outlook.com>
2016-04-30 22:43:39 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Install-ContainerHost/Install-ContainerHost.ps1 -OutFile c:\install-containerhost.ps1
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\install-containerhost.ps1"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
+Register-ScheduledTask -TaskName "scriptcontianers" -Action $action -Trigger $trigger -RunLevel Highest -User "system" | Out-Null
 Restart-Computer -Force
