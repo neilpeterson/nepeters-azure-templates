14daa57f32c90e3023c14af7da1760462aab1dde
Neil Peterson <neilpeterson@outlook.com>
2016-04-30 18:40:05 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -1,23 +1,25 @@
-$feature = Get-WindowsFeature -Name containers
+#$feature = Get-WindowsFeature -Name containers
 
-if ($feature.Installed -eq $false) {
+#if ($feature.Installed -eq $false) {
     
-    Install-WindowsFeature containers 
+#    Install-WindowsFeature containers 
     
     $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.8\Downloads\0\container.ps1"
     $trigger = New-ScheduledTaskTrigger -AtLogOn
     Register-ScheduledTask -TaskName "contianers" -Action $action -Trigger $trigger -RunLevel Highest | Out-Null
     Restart-Computer -Force      
-}
+#}
 
 # Unregister-ScheduledTask -TaskName contianers -Confirm:$false
 
 #if (!(Get-ContainerImage -Name windowsservercore)) {
 
-    Install-PackageProvider ContainerImage -Force
-    Install-ContainerImage -Name WindowsServerCore
+    #Install-PackageProvider ContainerImage -Force
+    #Install-ContainerImage -Name WindowsServerCore
 #}
 
 
 #Invoke-WebRequest https://aka.ms/tp5/Update-Container-Host -OutFile update-containerhost.ps1
 #.\update-containerhost.ps1
+sleep 300
+New-Item c:\test.txt
