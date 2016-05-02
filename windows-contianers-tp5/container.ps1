6b6cd4f5a88f49d1c29a1d6c13f00ee03f6ae836
Neil Peterson <neilpeterson@outlook.com>
2016-05-01 21:29:20 -0700

    Update container.ps1

--- windows-contianers-tp5/container.ps1
+++ windows-contianers-tp5/container.ps1
@@ -3,15 +3,17 @@
 #)
 
 function install-script {
-"# TP5 Contianer Installation`r`n
-    # Install Windows Server Core Image`r`n
-    Install-PackageProvider ContainerImage -Force`r
-    Install-ContainerImage -Name WindowsServerCore`r`n    
-    # Install Docker daemon and client`r`n
-    Invoke-WebRequest https://aka.ms/tp5/Update-Container-Host -OutFile update-containerhost.ps1`r
-    .\update-containerhost.ps1`r`n
-    docker tag windowsservercore:10.0.14300.1000 windowsservercore:latest`r`n
-    schtasks /DELETE /TN scriptcontianers /F
+"   
+# TP5 Contianer Installation`r
+# Install Windows Server Core Image`r`n
+Install-PackageProvider ContainerImage -Force`r
+Install-ContainerImage -Name WindowsServerCore`r`n    
+# Install Docker daemon and client`r`n
+Invoke-WebRequest https://aka.ms/tp5/Update-Container-Host -OutFile update-containerhost.ps1`r
+.\update-containerhost.ps1`r
+docker tag windowsservercore:10.0.14300.1000 windowsservercore:latest`r`n
+# Remove Scheduled Task`r`n
+schtasks /DELETE /TN scriptcontianers /F"
 }
 
 Install-Script > c:\images.ps1
