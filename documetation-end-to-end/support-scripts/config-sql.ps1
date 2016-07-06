faf4e5d977d527e5a9d9e45c550a2a30033c367f
Neil Peterson <neilpeterson@outlook.com>
2016-07-06 12:29:14 -0700

    Update config-sql.ps1

--- documetation-end-to-end/support-scripts/config-sql.ps1
+++ documetation-end-to-end/support-scripts/config-sql.ps1
@@ -5,8 +5,8 @@ Param (
 
 Import-Module SQLPS -DisableNameChecking
 $server = new-Object Microsoft.SqlServer.Management.Smo.Server("(local)")
-$login = new-object Microsoft.SqlServer.Management.Smo.Login("(local)", 'neillocal')
+$login = new-object Microsoft.SqlServer.Management.Smo.Login("(local)", $user)
 $login.LoginType = 'SqlLogin'
 $login.PasswordPolicyEnforced = $false
 $login.PasswordExpirationEnabled = $false
-$login.Create('Test123')
+$login.Create($password)
