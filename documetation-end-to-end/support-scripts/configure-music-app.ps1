e0e388d60628afdac4d9ecf02206e3944cb334f5
Neil Peterson <neilpeterson@outlook.com>
2016-07-10 22:03:22 -0700

    Update configure-music-app.ps1

--- documetation-end-to-end/support-scripts/configure-music-app.ps1
+++ documetation-end-to-end/support-scripts/configure-music-app.ps1
@@ -1,3 +1,4 @@
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
 New-Item -ItemType Directory c:\temp
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
 Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
