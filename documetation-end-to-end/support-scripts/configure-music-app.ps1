684f825e80ffe3f5fd27eb3c870caea43116011a
Neil Peterson <neilpeterson@outlook.com>
2016-07-11 09:22:37 -0700

    Update configure-music-app.ps1

--- documetation-end-to-end/support-scripts/configure-music-app.ps1
+++ documetation-end-to-end/support-scripts/configure-music-app.ps1
@@ -26,3 +26,9 @@ Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quie
 # download music app
 Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/documetation-end-to-end/music-app/music-store-azure-demo.zip -OutFile c:\temp\musicstore.zip
 Expand-Archive C:\temp\musicstore.zip c:\
+
+# start music store app
+Set-Location C:\music-store-azure-demo\src\MusicStore\
+& "C:\Program Files\dotnet\dotnet.exe" restore
+& "C:\Program Files\dotnet\dotnet.exe" run --server.urls=http://0.0.0.0:80
+
