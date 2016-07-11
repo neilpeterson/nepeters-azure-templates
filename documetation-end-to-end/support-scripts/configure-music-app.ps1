059a5e5fefa8486052af0a38e2dda334a45c835b
Neil Peterson <neilpeterson@outlook.com>
2016-07-11 16:08:10 -0700

    Update configure-music-app.ps1

--- documetation-end-to-end/support-scripts/configure-music-app.ps1
+++ documetation-end-to-end/support-scripts/configure-music-app.ps1
@@ -21,10 +21,15 @@ netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP
 New-Item -ItemType Directory c:\temp
 New-Item -ItemType Directory c:\music
 
+# install iis
+
+Install-WindowsFeature web-server -IncludeManagementTools
+
 # install dot.net core sdk
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
 Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
-
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
+Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
 # download music app
 Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/documetation-end-to-end/music-app/music-store-azure-demo.zip -OutFile c:\temp\musicstore.zip
@@ -33,6 +38,6 @@ Expand-Archive C:\temp\musicstore.zip c:\
 # start music store app
 Set-Location C:\music-store-azure-demo\src\MusicStore\
 & "C:\Program Files\dotnet\dotnet.exe" restore
-& "C:\Program Files\dotnet\dotnet.exe" build -o c:\music
+& "C:\Program Files\dotnet\dotnet.exe" publish -o c:\music
 #& "C:\Program Files\dotnet\dotnet.exe" run --server.urls=http://0.0.0.0:80
 
