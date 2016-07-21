40660f1fbbca666053b960760b39baa11cafe0a3
Neil Peterson <neilpeterson@outlook.com>
2016-07-21 11:53:01 -0700

    adding SQL DB music template

---
+++ dotnet-core-music-vm-sql-db/support-scripts/configure-music-app.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER Alias
+
+.EXAMPLE
+
+#>
+
+Param (
+
+)
+
+# firewall
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
+
+#folders
+
+New-Item -ItemType Directory c:\temp
+New-Item -ItemType Directory c:\music
+
+# install iis
+
+Install-WindowsFeature web-server -IncludeManagementTools
+
+# install dot.net core sdk
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
+Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
+Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
+
+# download music app
+Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/documetation-end-to-end/music-app/music-store-azure-demo.zip -OutFile c:\temp\musicstore.zip
+Expand-Archive C:\temp\musicstore.zip c:\
+
+# start music store app
+Set-Location C:\music-store-azure-demo\src\MusicStore\
+& "C:\Program Files\dotnet\dotnet.exe" restore
+& "C:\Program Files\dotnet\dotnet.exe" publish -o c:\music
+Remove-WebSite -Name "Default Web Site"
+Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
+New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
+& iisreset
+
