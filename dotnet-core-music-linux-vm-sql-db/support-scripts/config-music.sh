7a47cf3bb7951433aecb37c567909b660eb26d8f
Neil Peterson <neilpeterson@outlook.com>
2016-07-29 11:50:36 -0700

    Update config-music.sh

--- dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
+++ dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-NGINXCONFIG = "server {listen 80; location / {proxy_pass http://localhost:5000; proxy_http_version 1.1; proxy_set_header Upgrade $http_upgrade; proxy_set_header Connection keep-alive; proxy_set_header Host $host; proxy_cache_bypass $http_upgrade;}}"
-
-
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
