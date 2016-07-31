3b6bad6efc1c48270add8036146049e65b8c6753
Neil Peterson <neilpeterson@outlook.com>
2016-07-31 13:29:31 -0700

    app

--- dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
+++ dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# install NGINX
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo service nginx start
-
 # install dotnet core
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get install -y dotnet-dev-1.0.0-preview2-003121
 
-# sudo apt-get install -y git
-# sudo git clone https://github.com/neilpeterson/dotnet-linux-music.git /music
+# install NGINX
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo service nginx start
 
 # downlodd application
-#wget
+sudo wget https://raw.github.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/music-store-azure-demo.tar /
+sudo mkdir /music
+sudo tar -xf music-store-azure-demo.tar -C /music
 
 # upate config files
-#sudo rm -f /etc/nginx/sites-available/default
-#sudo wget https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/support-scripts/default /etc/nginx/sites-enabled/default
-#echo music
-#echo nginx
+sudo rm -f /etc/nginx/sites-available/default
+sudo cp /music/nginx-config/default /etc/nginx/sites-available/
+# need to add somehthing to update server, creds
 
 #start application
 #sudo nginx -s reload
