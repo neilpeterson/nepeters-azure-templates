6c54eb23bdf6d182e7dbf80379c74060900f362a
Neil Peterson <neilpeterson@outlook.com>
2016-08-15 12:30:42 -0700

    Testing dotnet publish

--- dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
+++ dotnet-core-music-linux-vm-sql-db/support-scripts/config-music.sh
@@ -11,26 +11,28 @@ sudo apt-get install -y nginx
 sudo service nginx start
 
 # download application
-sudo wget https://raw.github.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/music-store-azure-demo.tar /
+#sudo wget https://raw.github.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/music-store-azure-demo.tar /
+sudo wget https://raw.github.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/pub-music-store-azure-demo.tar /
 sudo mkdir /music
-sudo tar -xf music-store-azure-demo.tar -C /music
+sudo tar -xf pub-music-store-azure-demo.tar -C /music
 
 # update nginx config files
 sudo rm -f /etc/nginx/sites-available/default
 sudo cp /music/nginx-config/default /etc/nginx/sites-available/
 
-
 # update music config file
-sed -i "s/<replaceserver>/$1/g" /music/src/MusicStore/config.json
-sed -i "s/<replaceuser>/$2/g" /music/src/MusicStore/config.json
-sed -i "s/<replacepass>/$3/g" /music/src/MusicStore/config.json
- 
+#sed -i "s/<replaceserver>/$1/g" /music/src/MusicStore/config.json
+#sed -i "s/<replaceuser>/$2/g" /music/src/MusicStore/config.json
+#sed -i "s/<replacepass>/$3/g" /music/src/MusicStore/config.json
+sed -i "s/<replaceserver>/$1/g" /music/config.json
+sed -i "s/<replaceuser>/$2/g" /music/config.json
+sed -i "s/<replacepass>/$3/g" /music/config.json
 
 # start application
-sudo nginx -s reload
-sudo dotnet restore /music/src/MusicStore.Standalone
-cd /music/src/MusicStore.Standalone
-sudo dotnet run
+#sudo nginx -s reload
+#sudo dotnet restore /music/src/MusicStore.Standalone
+#cd /music/src/MusicStore.Standalone
+#sudo dotnet run
 
 # config supervisor
 sudo apt-get install -y supervisor
