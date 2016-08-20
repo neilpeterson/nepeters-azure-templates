cd5ad235da3c195aaf5a37709ce5810319af0d88
Neil Peterson <neilpeterson@outlook.com>
2016-08-19 21:32:50 -0700

    Update configure-music-app.ps1

--- dotnet-core-music-vm-sql-db/support-scripts/configure-music-app.ps1
+++ dotnet-core-music-vm-sql-db/support-scripts/configure-music-app.ps1
@@ -19,11 +19,12 @@ Param (
 # firewall
 netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
 
+# folders
+New-Item -ItemType Directory c:\temp
+New-Item -ItemType Directory c:\music
 
 # install iis
 Install-WindowsFeature web-server -IncludeManagementTools
-
-
 
 # install dot.net core sdk
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
@@ -32,12 +33,22 @@ Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\tem
 Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
 # download music app
-Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-vm-sql-db/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
-Expand-Archive C:\temp\musicstore.zip c:\music
+#Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-vm-sql-db/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
+#Expand-Archive C:\temp\musicstore.zip c:\music
+
+# download music app
+Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-all-vm/music-app/music-store-azure-demo.zip -OutFile c:\temp\musicstore.zip
+Expand-Archive C:\temp\musicstore.zip c:\
 
 # update SQL connection string
-Remove-Item c:\music\config.json
-$configfile = New-Item 'c:\music\config.json'
+#Remove-Item c:\music\config.json
+#$configfile = New-Item 'c:\music\config.json'
+#$configfile = New-Item 'c:\music-store-azure-demo\src\MusicStore\config.json'
+#$string = '{"AppSettings": { "SiteTitle": "ASP.NET MVC Music Store", "CacheDbResults": true }, "Data": { "DefaultConnection": { "ConnectionString": "Server=' + $sqlserver + ';Database=MusicStore;Integrated Security=False;User Id=' + $user + ';Password=' + $password + ';MultipleActiveResultSets=True;Connect Timeout=30" } } }'
+#Add-Content $configfile $string
+
+Remove-Item c:music-store-azure-demo\src\MusicStore\config.json
+$configfile = New-Item 'c:\music-store-azure-demo\src\MusicStore\config.json'
 #$configfile = New-Item 'c:\music-store-azure-demo\src\MusicStore\config.json'
 $string = '{"AppSettings": { "SiteTitle": "ASP.NET MVC Music Store", "CacheDbResults": true }, "Data": { "DefaultConnection": { "ConnectionString": "Server=' + $sqlserver + ';Database=MusicStore;Integrated Security=False;User Id=' + $user + ';Password=' + $password + ';MultipleActiveResultSets=True;Connect Timeout=30" } } }'
 Add-Content $configfile $string
@@ -47,6 +58,7 @@ Add-Content $configfile $string
 #& "C:\Program Files\dotnet\dotnet.exe" restore
 #& "C:\Program Files\dotnet\dotnet.exe" publish -o c:\music
 
+# Remove
 # update web.config
 #$current = "dotnet"
 #$replace = 'c:\Program Files\dotnet\dotnet.exe'
@@ -55,8 +67,17 @@ Add-Content $configfile $string
 #& iisreset
 
 # config iis
+#Remove-WebSite -Name "Default Web Site"
+#Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
+#New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
+#Start-Sleep 600
+#& iisreset
+
+# start music store app
+Set-Location C:\music-store-azure-demo\src\MusicStore\
+& "C:\Program Files\dotnet\dotnet.exe" restore
+& "C:\Program Files\dotnet\dotnet.exe" publish -o c:\music
 Remove-WebSite -Name "Default Web Site"
 Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
 New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
-Start-Sleep 600
 & iisreset
