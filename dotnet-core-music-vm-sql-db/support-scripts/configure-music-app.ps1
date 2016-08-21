9ec4fdec4f60ea98d4a4ae07b0248996673ffd14
Neil Peterson <neilpeterson@outlook.com>
2016-08-21 09:53:35 -0700

    script

--- dotnet-core-music-vm-sql-db/support-scripts/configure-music-app.ps1
+++ dotnet-core-music-vm-sql-db/support-scripts/configure-music-app.ps1
@@ -1,8 +1,3 @@
-<#
-.SYNOPSIS
-
-.DESCRIPTION
-
 .PARAMETER Alias
 
 .EXAMPLE
@@ -32,20 +27,19 @@ Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quie
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
 Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
-# download music app
-Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-vm-sql-db/music-app/music-store-azure-demo.zip -OutFile c:\temp\musicstore.zip
-Expand-Archive C:\temp\musicstore.zip c:\
+# download / config music app
+Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-vm-sql-db/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
+Expand-Archive C:\temp\musicstore.zip c:\music
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceserver>", $sqlserver } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceuser>", $user } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replacepass>", $password } | Set-Content C:\music\config.json
 
-Remove-Item c:\music-store-azure-demo\src\MusicStore\config.json
-$configfile = New-Item 'c:\music-store-azure-demo\src\MusicStore\config.json'
-$string = '{"AppSettings": { "SiteTitle": "ASP.NET MVC Music Store", "CacheDbResults": true }, "Data": { "DefaultConnection": { "ConnectionString": "Server=' + $sqlserver + ';Database=MusicStore;Integrated Security=False;User Id=' + $user + ';Password=' + $password + ';MultipleActiveResultSets=True;Connect Timeout=30" } } }'
-Add-Content $configfile $string
 
-# start music store app
-Set-Location C:\music-store-azure-demo\src\MusicStore\
-& "C:\Program Files\dotnet\dotnet.exe" restore
-Start-Process "C:\Program Files\dotnet\dotnet.exe" -ArgumentList "run"
-& "C:\Program Files\dotnet\dotnet.exe" publish -o c:\music
+# workaround for db creation bug
+Start-Process 'C:\Program Files\dotnet\dotnet.exe' -ArgumentList 'c:\music\MusicStore.dll'
+
+#configure iis
+
 Remove-WebSite -Name "Default Web Site"
 Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
 New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
