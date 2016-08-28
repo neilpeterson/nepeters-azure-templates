3e4a2ad3d485eca320505b9f6f984d4776893de5
Neil Peterson <neilpeterson@outlook.com>
2016-08-28 12:53:53 -0700

    Update swarm-mode.sh

--- ubuntu-docker-swarm-mode/support-scripts/swarm-mode.sh
+++ ubuntu-docker-swarm-mode/support-scripts/swarm-mode.sh
@@ -5,10 +5,11 @@
 sudo docker swarm init
 
 # get join command
-#joincommand=$(docker swarm join-token worker)
+#join="$(docker swarm join-token worker)"
+#newjoin=${join/To add a worker to this swarm, run the following command:}
 
 #sudo apt-get install sshpass
-#sshpass -p 'Monkeyskip76' ssh neillocal@10.0.0.4 'echo $(joincommand)'
+#sshpass -p 'Monkeyskip76' ssh neillocal@10.0.0.4 'echo $(newjoin)'
 
 # join node to swarm
 #ssh neillocal@10.0.0.4 'echo $(joincommand)'
