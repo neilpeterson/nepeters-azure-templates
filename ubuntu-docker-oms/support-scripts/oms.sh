3e2e3c13ec84c2bdf689bc199e026effe0fdcfa3
Neil Peterson <neilpeterson@outlook.com>
2016-10-18 09:39:01 -0700

    Update oms.sh

--- ubuntu-docker-oms/support-scripts/oms.sh
+++ ubuntu-docker-oms/support-scripts/oms.sh
@@ -3,7 +3,7 @@
 sudo wget https://github.com/MSFTOSSMgmt/OMS-Agent-for-Linux/releases/download/1.0.0-47/omsagent-1.0.0-47.universal.x64.sh
 sudo chmod +x ./omsagent-1.0.0-47.universal.x64.sh
 sudo md5sum ./omsagent-1.0.0-47.universal.x64.sh
-sudo ./omsagent-1.0.0-47.universal.x64.sh --install -w efd8d50c-42e7-4976-b36b-4f2440c12f05 -s pmZtZIPdKNSv7eAm6DhJG+GuEqNV6FXZLNmfCX3Y9fokUiVxAzh+7cPQc/BPm0VeTDVDLVY0qrQFN3fzbDnFnA==
+sudo ./omsagent-1.0.0-47.universal.x64.sh --install -w 87a96431-3755-4ac7-a9d9-4c47850f1704 -s BjJ+FD4vP+QknAEGqIjMoKoCFbLfqWxBtUWlgl4Ykobd7ammxfwcLvYWaW4fTpfhXa9lmNLDmCeZpJSS5bpfwg==
 
 sudo service omsagent restart
 DOCKER_OPTS="--log-driver=fluentd --log-opt fluentd-address=localhost:25225"
