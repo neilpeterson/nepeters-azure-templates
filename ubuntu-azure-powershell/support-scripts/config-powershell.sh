dccd6cf80d892493b9007b02bc1877f025cb71cd
neilpeterson <neilpeterson@outlook.com>
2017-01-08 09:45:42 -0800

    initial commit

---
+++ ubuntu-azure-powershell/support-scripts/config-powershell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install libunwind8 libicu52
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell_6.0.0-alpha.14-1ubuntu1.14.04.1_amd64.deb
+sudo dpkg -i powershell_6.0.0-alpha.14-1ubuntu1.14.04.1_amd64.deb
+sudo powershell
+Install module AzureRM.NetCore.Preview -Force
