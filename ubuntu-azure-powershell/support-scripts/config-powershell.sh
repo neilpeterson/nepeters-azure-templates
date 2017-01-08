9acad19950d8d3f3c30d82e4989f8c0c1815e7e7
Neil Peterson <neilpeterson@outlook.com>
2017-01-08 10:14:32 -0800

    Update config-powershell.sh

--- ubuntu-azure-powershell/support-scripts/config-powershell.sh
+++ ubuntu-azure-powershell/support-scripts/config-powershell.sh
@@ -4,4 +4,4 @@ sudo apt-get install libunwind8 libicu52 -y
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell_6.0.0-alpha.14-1ubuntu1.14.04.1_amd64.deb
 sudo dpkg -i powershell_6.0.0-alpha.14-1ubuntu1.14.04.1_amd64.deb
 sudo powershell
-Install module AzureRM.NetCore.Preview -Force
+Install-module AzureRM.NetCore.Preview -Force
