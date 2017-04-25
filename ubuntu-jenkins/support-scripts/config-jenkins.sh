635749a9abba98b16ff3a916e8aa247d2cc2e599
Neil Peterson <neilpeterson@outlook.com>
2017-04-25 10:23:33 -0700

    Update config-jenkins.sh

--- ubuntu-jenkins/support-scripts/config-jenkins.sh
+++ ubuntu-jenkins/support-scripts/config-jenkins.sh
@@ -5,7 +5,6 @@ sudo apt-get install nginx -y
 sudo apt-get install openjdk-7-jdk -y
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
 sudo apt-get install jenkins -y
 sudo apt-get install git -y
 sudo apt-get install python3-pip -y
