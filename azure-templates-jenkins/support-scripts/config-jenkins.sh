8e30a713f45740ffb888b0b6731560e044309f10
Neil Peterson <neilpeterson@outlook.com>
2018-01-20 23:17:30 -0800

    updated jenkins user

--- azure-templates-jenkins/support-scripts/config-jenkins.sh
+++ azure-templates-jenkins/support-scripts/config-jenkins.sh
@@ -13,7 +13,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
-sudo usermod -aG docker Jenkins
+sudo usermod -aG docker jenkins
 
 # Azure CLI
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
