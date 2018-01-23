b2f167440c8d5edbdc14405f27450eea2f55d744
Neil Peterson <neilpeterson@outlook.com>
2018-01-22 21:01:50 -0800

    script updates

--- azure-templates-jenkins/support-scripts/config-jenkins.sh
+++ azure-templates-jenkins/support-scripts/config-jenkins.sh
@@ -4,7 +4,7 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 # Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
